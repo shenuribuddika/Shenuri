d2a10e9c8dc1883d35815377905d176f5f973735
Shenuri Buddika <shenuribuddika@gmail.com>
2026-06-28 21:22:02 +0530

    Dark / Light Mode

--- script.js
+++ script.js
@@ -40,3 +40,15 @@ function typeEffect() {
 }
 
 typeEffect();
+
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+        toggleBtn.innerHTML = "🌙Dark Mode";
+    }else{
+        toggleBtn.innerHTML = "☀️Light Mode";
+    }
+});
